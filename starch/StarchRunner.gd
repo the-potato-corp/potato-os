4d16476969335f85a22f82a0b2051c965f1ef329
nisse <nisse.malmstrom@icloud.com>
2025-11-16 13:27:43 +0100

    made a starch CLI and LSP to later be used in different editor extensions.

--- starch/StarchRunner.gd
+++ starch/StarchRunner.gd
@@ -8,32 +8,35 @@ func _init(location: String) -> void:
 func run() -> void:
 	var code: String = FileAccess.get_file_as_string(location)
 	code = code.replace("\r\n", "\n") # I FRICKING HATE UNIX/WINDOWS DIFFERENCES GOD THIS TOOK LITERAL HOURS TO RESOLVE
+	# (nissemanen) why dont you just make the lexer (or what ever has problems with them) treat them both ("\r\n" and "\n") the same way?
 	
 	var lexer: Lexer = Lexer.new(code)
 	var error: Error = lexer.lex()
 	
 	if error != OK:
 		print("Lex Error: %s" % lexer.get_error())
-	else:
-		var tokens: Array[Token] = lexer.get_tokens()
-		#for token in tokens:
-			#print(token.value)
-		# Parsing and eval here
-		var parser: Parser = Parser.new(tokens)
-		
-		error = parser.parse()
-		
-		if error != OK:
-			print("Parse Error: %s" % parser.get_error())
-		else:
-			var program: Program = parser.get_program()
-			for s in program.statements:
-				print(s.serialise())
-			
-			var interpreter: Interpreter = Interpreter.new()
-			interpreter.set_file_path("res://starch/main.starch")
-			print()
-			var result: EvalError = interpreter.run(program)
-			
-			if result.type != OK:
-				print("Eval Error: %s" % result.message)
+		return
+	
+	var tokens: Array[Token] = lexer.get_tokens()
+	#for token in tokens:
+		#print(token.value)
+	# Parsing and eval here
+	var parser: Parser = Parser.new(tokens)
+	
+	error = parser.parse()
+	
+	if error != OK:
+		print("Parse Error: %s" % parser.get_error())
+		return
+	
+	var program: Program = parser.get_program()
+	for s in program.statements:
+		print(s.serialise())
+	
+	var interpreter: Interpreter = Interpreter.new()
+	interpreter.set_file_path("res://starch/main.starch")
+	print()
+	var result: EvalError = interpreter.run(program)
+	
+	if result.type != OK:
+		print("Eval Error: %s" % result.message)
